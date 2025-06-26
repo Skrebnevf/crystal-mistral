5d70c4ef5807bfc22308a897ade034b048d27a1b
f.skrebnev <f.skrebnev@getic.com>
2025-06-26 20:02:25 +0200

    add tests for code and chat

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,11 @@
 require "spec"
 require "../src/crystal-mistral"
+
+struct TestCase
+  property name : String
+  property input : Array(Messages) | String
+  property model : String
+
+  def initialize(@name, @input, @model)
+  end
+end
